e3f1911d01e82bee75147cde075aa40b3e91ce07
Logan Godfrey <lgodfrey@student.csdspartans.org>
2017-03-07 09:21:22 -0500

    save

--- readprintfiles/readprintwholedocnoblnklines.py
+++ readprintfiles/readprintwholedocnoblnklines.py
@@ -4,9 +4,22 @@
 name_of_mydocument = 'mydocument.txt'
 file_input = open(name_of_mydocument, 'r')     #open file for reading
 
+line_counter=0
+
 line = file_input.readline()
+line = file_input.readline()
+line = file_input.readline()
+line = file_input.readline()
+line = file_input.readline()
+line = file_input.readline()
+line = file_input.readline()
+line = file_input.readline()
+line = file_input.readline()
+
+
 while line != '':                      # while not end of file
-    print(line, end = '')              # don't print another new line
+    line_counter +=1
+    print(line_counter, line, end = '')              # don't print another new line
     line = file_input.readline()
 
-file_input.close()
+file_input.close() 
